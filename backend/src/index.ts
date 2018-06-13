74da02df6523308838a31f7f106a9d4d7e861441
Benjamin Albrecht <benjamin.albrecht@gmail.com>
2018-06-13 16:25:25 +0200

    First executable version of the backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,53 @@
 import express = require("express");
-
 import { log } from "./Logger";
-
-
-
+import fs = require("fs");
 
 const app = express();
 
 // Configure REST Server
-app.get("/vongpiler/:source", (request, response) => {
+app.use(express.json());
+app.post("/vongline", (request, response) => {
     log.debug("Request: " + request.url);
-    // tslint:disable-next-line:prefer-const
-    let params = request.params;
-    const query = request.query;
-
-    if (!isNaN(params)) {
+    if (!isNaN(request.params)) {
         response
             .status(400)
             .send("No string as name");
 
-    } else {
-        response.json(
-        
-            );
+    } else if (request.body !== null){
+        const filename = Math.floor(Math.random() * (0 - 100000 + 1)) + 100000;
+        log.debug(request.body);
+
+        fs.writeFile('tmp/' + filename + ".vsh", request.body.Code.toString(), function(err) {
+            if(err) {
+                log.error(err.toString());
+            }
+            log.info(filename + ".vsh saved");
+      
+            var exec = require('child_process').exec, child;
+            child =  exec('java -jar ./java/vongc.jar ./tmp/' + filename +'.vsh', function (error:any, stdout:any, stderr:any){
+                log.info("vongc.jar stdout: " + stdout);
+                if(stdout !== null){
+                    child =  exec('java -jar ./java/vong.jar ./tmp/' + filename +'.vch', function (error:any, stdout:any, stderr:any){
+                        log.info("vong.jar stdout: " + stdout);
+                        response.json(
+                            {"Response":stdout},
+                        );
+                        if(stderr){
+                            log.error("vong.jar stderr: " + stderr);
+                        }
+                        if(error !== null){
+                            log.error("vong.jar exec error: " + error);
+                        }
+                    });
+                }
+                if(stderr){
+                    log.error("vongc.jar stderr: " + stderr);
+                }
+                if(error !== null){
+                    log.error("vongc.jar exec error: " + error);
+                }
+            });
+         });
     }
 });
 
