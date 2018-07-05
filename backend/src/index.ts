d9c970b10c43039e9a12e2a4cfdb8e796a89715e
Benjamin Albrecht <benjamin.albrecht@gmail.com>
2018-07-05 18:16:06 +0200

    Littel fixs in the URI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,18 +3,16 @@ import fs = require("fs");
 import https = require("https");
 import { log } from "./Logger";
 
-
-
 const app = express();
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/yourdomain.com/chain.pem', 'utf8');
+const privateKey = fs.readFileSync("/etc/letsencrypt/live/vongpiler.de/privkey.pem", "utf8");
+const certificate = fs.readFileSync("/etc/letsencrypt/live/vongpiler.de/cert.pem", "utf8");
+const ca = fs.readFileSync("/etc/letsencrypt/live/vongpiler.de/chain.pem", "utf8");
 
 const credentials = {
-	key: privateKey,
-	cert: certificate,
+  key: privateKey,
+  cert: certificate,
 	ca: ca
 };
 
@@ -37,7 +35,6 @@ app.post("/compile", (request: any, response: any) => {
         let sessionId: number;
         let compiled = false;
 
-
         if (request.body.SessionId !== undefined) {
           sessionId = request.body.SessionId;
 
@@ -54,19 +51,21 @@ app.post("/compile", (request: any, response: any) => {
             log.info(sessionId + ".vsh saved");
 
             const exec = require("child_process").exec;
-            exec("docker exec vongline_compiler java -jar java/vongc.jar tmp/vsh/" + sessionId + ".vsh -o tmp/vch/" + sessionId + ".vch ",
+            exec("docker exec vongline_compiler java -jar java/vongc.jar tmp/vsh/"
+            + sessionId + ".vsh -o tmp/vch/" + sessionId + ".vch ",
             (errorC: any, stdoutC: any, stderrC: any) => {
                 log.info("vongc.jar stdout: " + stdoutC);
 
-                let regExp = new RegExp("(vong\.piler\.her\.generator\.ByteCodeWriter in main: vong class her was successfully written)");
-                compiled = regExp.test(stdoutC);                
+                const regExp = new RegExp("(vong\.piler\.her\.generator\.ByteCodeWriter in main: "
+                + "vong class her was successfully written)");
+                compiled = regExp.test(stdoutC);
 
                 response.json({
                     Compiled: compiled,
                     SessionId: sessionId,
-                    StdoutC: stdoutC                           
+                    StdoutC: stdoutC,
                     },
-                );                
+                );
                 if (stderrC) {
                     log.error("vongc.jar stderr: " + stderrC);
                 }
@@ -92,8 +91,8 @@ app.post("/exec", (request: any, response: any) => {
           log.debug("SessionId: " + sessionId);
 
           const exec = require("child_process").exec;
-          exec("docker run --rm -v /tmp/vongline/vch:/app/tmp/vch:ro --name vongline_runtime-" + sessionId
-              +" vongline_runtime java -jar java/vong.jar tmp/vch/" + sessionId + ".vch",
+          exec("docker run --rm -v /tmp/vongline/vch:/app/tmp/vch:ro --name vongline_runtime-"
+          + sessionId + " vongline_runtime java -jar java/vong.jar tmp/vch/" + sessionId + ".vch",
               (errorR: any, stdoutR: any, stderrR: any) => {
               log.info("vong.jar stdout: " + stdoutR);
               response.json({
@@ -110,11 +109,11 @@ app.post("/exec", (request: any, response: any) => {
           });
 
         } else {
-           response
-          .status(400)
-          .send("SessionId not available");
-          log.debug("SessionId not available");
-        }       
+            response
+            .status(400)
+            .send("SessionId not available");
+            log.debug("SessionId not available");
+        }
     }
 });
 
@@ -122,5 +121,5 @@ const httpsServer = https.createServer(credentials, app);
 
 // Start REST server
 httpsServer.listen(8443, () => {
-	log.info('HTTPS REST Server running on port 8443');
+  log.info("HTTPS REST Server running on port 8443");
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { DiffEditorModel, NgxEditorModel } from '../platform/editor';
 import { HttpClient } from '@angular/common/http';
 
-const url:string = "https://vongpiler:8443/";
+const url:string = "https://vongpiler.de:8443/";
 
 @Component({
   selector: 'app-root',
@@ -45,7 +45,7 @@ export class AppComponent {
           this.compiled = buffer.Compiled;
         },
         err => {
-          this.result = "Connection error: " + JSON.stringify(err);
+          this.result = "Connection error to backend";
         }
       );
   }
@@ -57,7 +57,7 @@ export class AppComponent {
         this.sessionId = buffer.SessionId;
       },
       err => {
-        this.result = "Connection error: " + JSON.stringify(err);
+        this.result = "Connection error to backend";
       }
     );
 }
